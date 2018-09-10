7a310d4a744eab786d5517327936e12a4f6c2e37
Jongse Park <jspark@gatech.edu>
2018-09-10 12:38:14 -0700

    program makefile added

---
+++ vivado/build.sh
@@ -0,0 +1,3 @@
+make program BITFILE=../bitfiles/ku115_wrapper_yolo-32x32-150MHz-hcdemo-v1.0.bit
+make program BITFILE=../bitfiles/ku115_wrapper_yolo-32x32-150MHz-hcdemo-v1.0.bit
+make program BITFILE=../bitfiles/ku115_wrapper_yolo-32x32-150MHz-hcdemo-v1.0.bit
